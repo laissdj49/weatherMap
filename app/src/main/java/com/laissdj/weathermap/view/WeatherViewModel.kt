069ef863b1a7e55a131268979e38446c1e819a25
lais santos <laissdj49@gmail.com>
2024-11-21 11:24:37 +0100

    Create WeatherScreen and viewModel

--- app/src/main/java/com/laissdj/weathermap/view/WeatherViewModel.kt
+++ app/src/main/java/com/laissdj/weathermap/view/WeatherViewModel.kt
@@ -3,22 +3,42 @@ package com.laissdj.weathermap.view
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.laissdj.weathermap.repository.WeatherRepository
+import com.laissdj.weathermap.view.WeatherError.Companion.fromException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class WeatherViewModel(private val repository: WeatherRepository) : ViewModel() {
 
-    private val _weatherMap: MutableStateFlow<WeatherPresentation> =
-        MutableStateFlow(WeatherPresentation())
-    val weatherMap: StateFlow<WeatherPresentation> = _weatherMap
+    private val _weatherMap: MutableStateFlow<WeatherState> =
+        MutableStateFlow(WeatherState.Start)
+    val weatherMap: StateFlow<WeatherState> = _weatherMap
 
     fun weatherMap(value: String) {
         viewModelScope.launch {
             runCatching {
-                val result = repository.getWeather(value)
-                val domain = WeatherMapDomain()
-                _weatherMap.value = domain.mapToPresentation(result)
+                _weatherMap.value = WeatherState.Loading
+
+
+                val response = repository.getWeather(value)
+                _weatherMap.value = response.mapToPresentation()
+
+            }.onFailure { e ->
+                e.printStackTrace()
+                _weatherMap.value = WeatherState.Error(fromException(exception = e))
+            }
+        }
+    }
+
+    fun weatherLocation(lat: Double, lon: Double) {
+        viewModelScope.launch {
+            runCatching {
+                _weatherMap.value = WeatherState.Loading
+                val response = repository.getWeather(lat, lon)
+                _weatherMap.value = response.mapToPresentation()
+            }.onFailure { e ->
+                e.printStackTrace()
+                _weatherMap.value = WeatherState.Error(fromException(e))
             }
         }
     }
